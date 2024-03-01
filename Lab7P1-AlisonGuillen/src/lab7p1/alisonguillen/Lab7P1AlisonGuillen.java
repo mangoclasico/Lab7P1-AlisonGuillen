689c55734117fea255a744ded1ff4d3549dbf6fc
unknown <alisonguillen@outlook.com>
2024-03-01 14:01:57 -0600

    Primer commit

--- Lab7P1-AlisonGuillen/src/lab7p1/alisonguillen/Lab7P1AlisonGuillen.java
+++ Lab7P1-AlisonGuillen/src/lab7p1/alisonguillen/Lab7P1AlisonGuillen.java
@@ -4,6 +4,7 @@
  */
 package lab7p1.alisonguillen;
 
+import java.util.Scanner;
 /**
  *
  * @author Dell i7
@@ -14,7 +15,28 @@ public class Lab7P1AlisonGuillen {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner rd = new Scanner(System.in);
+        int respuesta = 1;
+
+        while (respuesta != 4) {
+            System.out.println("\nMenu");
+            System.out.println("1. Numero equilibrado");
+            System.out.println("2. Numero solitario");
+            System.out.println("3. Intercambio de filas y/o columnas");
+            System.out.println("4. Salir");
+            System.out.print("Ingrese su opcion: ");
+
+            int opcionmenu = rd.nextInt();
+            rd.nextLine(); 
+
+            switch (opcionmenu) {
+                case 1:
+                    System.out.println("Ingrese cantidad de filas: ");
+                    int filas = rd.nextInt();
+                    System.out.println("Ingrese cantidad de columnas: ");
+                    int col = rd.nextInt();
     }
     
+        }
+    }
 }
